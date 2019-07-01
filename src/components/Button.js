e5114fcfdf520e9b14f359bcac0c0e8c5ea037fd
Ricardo Carballo <rcarbaleq2@gmail.com>
2019-07-01 08:15:59 -0700

    Creatin Context Objects

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,6 +1,9 @@
 import React from 'react';
+import LanguageContext from '../contexts/LanguageComponent';
 
 class Button extends React.Component{
+    static contextType = LanguageContext;
+
     render(){
         return (
             <button className="ui button primary">
